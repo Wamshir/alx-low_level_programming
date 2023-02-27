b11d77f3ca5f8ac12d23dfa0e526ec60863ec8e6
Wamshir <clintonshiroya@gmail.com>
2023-02-27 14:29:19 -0800

    Trevo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to a int as parameter
+ * and updates the value on the pointer to 9
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
