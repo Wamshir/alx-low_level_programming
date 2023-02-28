00270429d24351cd64e86e349a15d6048ccc03b6
Wamshir <clintonshiroya@gmail.com>
2023-02-27 17:22:34 -0800

    Layman

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination Value
+ * @src: Source Value
+ * Return: The pointer destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
